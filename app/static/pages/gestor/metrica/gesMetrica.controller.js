479509729df7b294ebba7512d34640ad4a26373c
Luis Antonio García Perrusquía <lgarciaperrusquia@gmail.com>
2018-08-07 20:15:14 -0500

    chartExample

--- app/static/pages/gestor/metrica/gesMetrica.controller.js
+++ app/static/pages/gestor/metrica/gesMetrica.controller.js
@@ -1,14 +1,52 @@
-app.controller("gesMetricaCtrl", ["$scope", "$sce", "$location","filterFilter", "gesMetricaFactory", function($scope, gesMetricaFactory ) {
-    // $scope.DataUser     	= JSON.parse( localStorage.getItem("RCVUserData") );
-    $scope.editorOptions = {
-	    // settings more at http://docs.ckeditor.com/#!/guide/dev_configuration
-    };
-    
-    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-    $scope.series = ['Series A', 'Series B'];
-  
-    $scope.data = [
-      [65, 59, 80, 81, 56, 55, 40],
-      [28, 48, 40, 19, 86, 27, 90]
-    ];
+app.controller("gesMetricaCtrl", ["$scope", "$sce", "$location", "filterFilter", "gesMetricaFactory", function ($scope, gesMetricaFactory) {
+	// $scope.DataUser     	= JSON.parse( localStorage.getItem("RCVUserData") );
+	$scope.editorOptions = {
+		// settings more at http://docs.ckeditor.com/#!/guide/dev_configuration
+	};
+
+	optionsPie = {
+		title: {
+			text: "Hola"
+		},
+		data: [{
+			type: "pie",
+			startAngle: 45,
+			showInLegend: "true",
+			legendText: "{label}",
+			indexLabel: "{label} ({y})",
+			yValueFormatString: "#,##0.#" % "",
+			dataPoints: [
+				{ label: "Organic", y: 36 },
+				{ label: "Email Marketing", y: 31 },
+				{ label: "Referrals", y: 7 },
+				{ label: "Twitter", y: 7 },
+				{ label: "Facebook", y: 6 },
+				{ label: "Google", y: 10 },
+				{ label: "Others", y: 3 }
+			]
+		}]
+	};
+	
+	optionsBar = {
+		title: {
+			text: "Column Chart in jQuery CanvasJS"              
+		},
+		data: [              
+		{
+			// Change type to "doughnut", "line", "splineArea", etc.
+			type: "column",
+			dataPoints: [
+				{ label: "apple",  y: 10  },
+				{ label: "orange", y: 15  },
+				{ label: "banana", y: 25  },
+				{ label: "mango",  y: 30  },
+				{ label: "grape",  y: 28  }
+			]
+		}
+		]
+	};
+	
+	$("#barContainer").CanvasJSChart(optionsBar);
+	
+	$("#pieContainer").CanvasJSChart(optionsPie);
 }]);
