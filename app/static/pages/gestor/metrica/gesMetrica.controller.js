e236cc6057321a9405f8901f89f8a163a124c134
Luis Antonio García Perrusquía <lgarciaperrusquia@gmail.com>
2018-08-07 19:47:55 -0500

    Routing

--- app/static/pages/gestor/metrica/gesMetrica.controller.js
+++ app/static/pages/gestor/metrica/gesMetrica.controller.js
@@ -1,8 +1,14 @@
-app.controller("gesMetricaCtrl", ["$scope", "$sce", "$location","filterFilter","gesTemaFactory", function($scope, $sce, $location, filterFilter, gesTemaFactory ) {
+app.controller("gesMetricaCtrl", ["$scope", "$sce", "$location","filterFilter", "gesMetricaFactory", function($scope, gesMetricaFactory ) {
     // $scope.DataUser     	= JSON.parse( localStorage.getItem("RCVUserData") );
     $scope.editorOptions = {
 	    // settings more at http://docs.ckeditor.com/#!/guide/dev_configuration
-	};
-
-	
+    };
+    
+    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    $scope.series = ['Series A', 'Series B'];
+  
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
 }]);
