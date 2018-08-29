532ad1fa6e55a54fad29a2828c2481b10ebf608e
Luis Antonio García Perrusquía <lgarciaperrusquia@gmail.com>
2018-08-29 17:48:22 -0500

    CRUD Fuente

--- app/static/pages/gestor/fuente/gesFuente.factory.js
+++ app/static/pages/gestor/fuente/gesFuente.factory.js
@@ -1,4 +1,5 @@
 var Path_Favorito      = API_Path + '/enlaces/';
+var Path_Fuente        = API_Path + '/fuente/';
 
 app.factory( 'gesFuenteFactory', function( $http ){
 	return {
@@ -8,6 +9,44 @@ app.factory( 'gesFuenteFactory', function( $http ){
                 method: "GET",
                 params: {
                     idUsuario: idUsuario
+                },
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        },
+        insertFuente: function( idUsuario, fuente ) {
+            return $http({
+                url: Path_Fuente + 'insertFuente/',
+                method: "GET",
+                params: {
+                    idUsuario: idUsuario,
+                    fuente: fuente
+                },
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        },
+        deleteFuente: function( idFuente ) {
+            return $http({
+                url: Path_Fuente + 'deleteFuente/',
+                method: "GET",
+                params: {
+                    idFuente: idFuente
+                },
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        },
+        editFuente: function( idFuente, fuente ) {
+            return $http({
+                url: Path_Fuente + 'editFuente/',
+                method: "GET",
+                params: {
+                    idFuente: idFuente,
+                    fuente: fuente
                 },
                 headers: {
                     'Content-Type': 'application/json'
